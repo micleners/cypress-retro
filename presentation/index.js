dcb960e0b221a214d8bbe518501df9c9cfb76c6f
Michael Leners <micleners@gmail.com>
2020-01-07 13:10:12 -0600

    Update for time being

--- presentation/index.js
+++ presentation/index.js
@@ -1,5 +1,5 @@
 // Import React
-import React from 'react';
+import React from "react";
 
 // Import Spectacle Core tags
 import {
@@ -14,64 +14,78 @@ import {
   Quote,
   Slide,
   Text
-} from 'spectacle';
+} from "spectacle";
 
 // Import theme
-import createTheme from 'spectacle/lib/themes/default';
+import createTheme from "spectacle/lib/themes/default";
 
 const images = {
-  formidagon: require('../assets/formidable-logo.svg'),
-  goodWork: require('../assets/good-work.gif')
+  formidagon: require("../assets/formidable-logo.svg"),
+  goodWork: require("../assets/good-work.gif")
 };
 
 // Require CSS
-require('normalize.css');
+require("normalize.css");
 
 const theme = createTheme(
   {
-    dark: '#1C1B21',
-    text: '#BBBBBB',
-    tertiary: '#EAEAEA',
-    quaternary: '#3D4554',
-    accentGreen: '#26D9D2',
-    accentRed: '#EF5350',
-    accentBlue: '#1876D2'
+    dark: "#1C1B21",
+    text: "#BBBBBB",
+    tertiary: "#EAEAEA",
+    quaternary: "#3D4554",
+    accentGreen: "#26D9D2",
+    accentRed: "#EF5350",
+    accentBlue: "#1876D2"
   },
   {
     primary: {
-      name: 'Oxygen Mono',
+      name: "Oxygen Mono",
       googleFont: true,
-      styles: ['400', '700i']
+      styles: ["400", "700i"]
     },
     secondary: {
-      name: 'Roboto Mono',
+      name: "Roboto Mono",
       googleFont: true,
-      styles: ['400', '700i']
+      styles: ["400", "700i"]
     }
   }
 );
-
 
 export default class Presentation extends React.Component {
   render() {
     return (
       <Deck
-        transition={['zoom', 'slide']}
+        transition={["zoom", "slide"]}
         transitionDuration={500}
+        contentWidth={1400}
         theme={theme}
       >
-        <Slide transition={['zoom']} bgColor="dark">
-          <Heading size={1} fit caps lineHeight={1} textColor="text">
-            Spectacle Boilerplate
+        <Slide transition={["zoom"]} bgColor="dark">
+          <Heading size={2} lineHeight={1.1} textColor="accentGreen">
+            Cypress for E2E Testing:
           </Heading>
-          <Text margin="10px 0 0" textColor="tertiary" fit bold>
-            open the presentation/index.js file to get started
-          </Text>
+          <Heading size={3} lineHeight={1.1} textColor="accentGreen">
+            A Retrospective
+          </Heading>
+          <br></br>
+          <Heading size={4} caps lineHeight={1} textColor="tertiary">
+            Michael Leners
+          </Heading>
+          <Heading size={4} lineHeight={1} textColor="tertiary">
+            @micLeners
+          </Heading>
+          <br></br>
+          <Heading size={4} lineHeight={1} textColor="accentBlue">
+            dsmJS @ Source Allies
+          </Heading>
+          <Heading size={4} caps lineHeight={1} textColor="accentBlue">
+            January 14, 2020
+          </Heading>
         </Slide>
-        <Slide bgColor="text">
+        {/* <Slide bgColor="text">
           <Image src={images.formidagon} width={800} />
         </Slide>
-        <Slide transition={['fade']} bgColor="tertiary">
+        <Slide transition={["fade"]} bgColor="tertiary">
           <Heading size={6} textColor="dark" caps>
             Typography
           </Heading>
@@ -94,7 +108,7 @@ export default class Presentation extends React.Component {
             Standard text
           </Text>
         </Slide>
-        <Slide transition={['fade']} bgColor="dark" textColor="tertiary">
+        <Slide transition={["fade"]} bgColor="dark" textColor="tertiary">
           <Heading size={6} textColor="text" caps>
             Standard List
           </Heading>
@@ -105,7 +119,7 @@ export default class Presentation extends React.Component {
             <ListItem>Item 4</ListItem>
           </List>
         </Slide>
-        <Slide transition={['fade']} bgColor="text" textColor="dark">
+        <Slide transition={["fade"]} bgColor="text" textColor="dark">
           <BlockQuote>
             <Quote>Example Quote</Quote>
             <Cite margin="10px 0 0 30px">Author</Cite>
@@ -114,7 +128,7 @@ export default class Presentation extends React.Component {
         <Slide>
           <Image src={images.goodWork} width={500} />
           <Notes>gifs work too</Notes>
-        </Slide>
+        </Slide> */}
       </Deck>
     );
   }
